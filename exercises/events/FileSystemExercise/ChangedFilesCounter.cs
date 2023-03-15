d3bb6e5909acb6dee78bb471773d2cbbe8bfa383
Ferre Laridon <ferre.laridon@student.ucll.be>
2023-03-15 11:40:59 +0100

    finished FileSystemExercise, PersonExercise, TimeExercise

---
+++ exercises/events/FileSystemExercise/ChangedFilesCounter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace FileSystemExercise
+{
+    internal class ChangedFilesCounter
+    {
+
+        private HashSet<string> files;
+
+        public ChangedFilesCounter(IFileSystem fileSystem)
+        {
+            this.files = new HashSet<string>();
+
+            fileSystem.FileCreated += OnFileModified;
+            fileSystem.FileDeleted += OnFileModified;
+            fileSystem.FileWritten += OnFileModified;
+        }
+
+        public int ChangeCount => files.Count;
+
+        public void OnFileModified(string path)
+        {
+            files.Add(path);
+        }
+    }
+}
---
+++ exercises/properties/PersonExercise/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonExercise
+{
+    internal class Person
+    {
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name may not be empty!");
+                }
+                this.name = value;
+            }
+        }
+
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value <= 0) 
+                {
+                    throw new ArgumentException("Age must be strictly positive!");
+                }
+                this.age = value;
+            }
+        }
+
+    }
+}
